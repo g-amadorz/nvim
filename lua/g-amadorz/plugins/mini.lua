18e87f2c87766a8ab1226cfda4599c072191e552
Gabriel Amador Zarza <gabe.amadorz@gmail.com>
2025-10-28 07:53:31 -0700

    smear fix

--- lua/g-amadorz/plugins/mini.lua
+++ lua/g-amadorz/plugins/mini.lua
@@ -240,13 +240,21 @@ return {
 
 	{
 		"sphamba/smear-cursor.nvim",
+		event = "VeryLazy",
+		cond = vim.g.neovide == nil,
 		opts = {
-			stiffness = 0.6, -- 0.6      [0, 1]
-			trailing_stiffness = 0.6, -- 0.45     [0, 1]
-			stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			distance_stop_animating = 0.1, -- 0.1      > 0
-			use_floating_windows = false,
+			hide_target_hack = true,
+			cursor_color = "none",
+		},
+		specs = {
+			-- disable mini.animate cursor
+			{
+				"nvim-mini/mini.animate",
+				optional = true,
+				opts = {
+					cursor = { enable = false },
+				},
+			},
 		},
 	},
 }
